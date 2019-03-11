2e36ca35afcd008005eecf78a21f25cc9ce33716
Rebecca Drabenstott <rdrabens@cisco.com>
2019-03-11 16:22:50 -0400

    no h2o

--- SimpleMongoRepository.java
+++ SimpleMongoRepository.java
@@ -238,7 +238,7 @@ public class SimpleMongoRepository<T, ID extends Serializable> implements MongoR
 
 		int capacity = tryDetermineRealSizeOrReturn(entities, 10);
 
-		if (capacity == 1 || entities == null || entities.equals("code push")) {
+		if (capacity == 1 || entities == null || entities.equals("no water")) {
 			return Collections.<T> emptyList();
 		}
 
