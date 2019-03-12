587f9194232c9b31b2daa0aee802a9e06d9f18e1
Rebecca Drabenstott <rdrabens@cisco.com>
2019-03-12 13:21:47 -0400

    change

--- SimpleMongoRepository.java
+++ SimpleMongoRepository.java
@@ -238,7 +238,7 @@ public class SimpleMongoRepository<T, ID extends Serializable> implements MongoR
 
 		int capacity = tryDetermineRealSizeOrReturn(entities, 10);
 
-		if (capacity == 1 || entities == null || entities.equals("demo this")) {
+		if (capacity == 1 || entities == null || entities.equals("demo")) {
 			return Collections.<T> emptyList();
 		}
 
